b971e57ac14d947d512b996cd0966fe496011ae4
Ethan Saw <ethansaw@gmail.com>
2024-10-06 05:07:41 -0700

    add animated background and update title

--- scanecipe/src/index.js
+++ scanecipe/src/index.js
@@ -8,9 +8,12 @@ import OpenAI from "./components/openai";
 import { IngredientsProvider } from "./IngredientsContext"; // Import the context provider
 import { MotionProvider } from "./MotionContext";
 
+import { AnimatedBackground } from 'animated-backgrounds';
+
 const App = () => {
   return (
     <div className="app">
+      <AnimatedBackground animationName="fallingFoodFiesta" />
       <header>
         <h1 
           style={{
@@ -18,18 +21,20 @@ const App = () => {
             fontWeight: 'bold', 
             textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
             transition: 'transform 0.3s, color 0.3s',
-            cursor: 'pointer' 
+            cursor: 'pointer' ,
+            transform: 'scale(1.1)',
+            color: '#ff6a00'
           }}
-          onMouseOver={(e) => {
-            e.currentTarget.style.transform = 'scale(1.1)';
-            e.currentTarget.style.color = '#ff6a00';
-          }}
-          onMouseOut={(e) => {
-            e.currentTarget.style.transform = 'scale(1)';
-            e.currentTarget.style.color = '';
-          }}
+          // onMouseOver={(e) => {
+          //   e.currentTarget.style.transform = 'scale(1.1)';
+          //   e.currentTarget.style.color = '#ff6a00';
+          // }}
+          // onMouseOut={(e) => {
+          //   e.currentTarget.style.transform = 'scale(1)';
+          //   e.currentTarget.style.color = '';
+          // }}
         >
-          BarcodeBytes
+          🍦🍩🍣🌮Barcode🥓Bytes🍔🍕🌭🍟
       </h1>
     </header>
     <IngredientsProvider>
